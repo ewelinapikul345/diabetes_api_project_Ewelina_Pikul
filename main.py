283550142e2c9ff535545ad054afed7fe24a4362
ewelinapikul345 <ewelina.pikul345@gmail.com>
2026-06-24 21:31:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import joblib
 import numpy as np
 
 app = FastAPI(title="Diabetes Prediction API")
-model = tf.keras.models.load_model('diabetes_model.keras')
+model = tf.saved_model.load('diabetes_model_saved')
 scaler = joblib.load('scaler.pkl')
 
 class PatientData(BaseModel):
@@ -21,11 +21,16 @@ class PatientData(BaseModel):
 @app.post("/predict")
 def predict_diabetes(data: PatientData):
     input_data = np.array([[
-        data.Pregnancies, data.Glucose, data.BloodPressure,
-        data.SkinThickness, data.Insulin, data.BMI,
+        data.Pregnancies, data.Glucose, data.BloodPressure, 
+        data.SkinThickness, data.Insulin, data.BMI, 
         data.DiabetesPedigreeFunction, data.Age
-    ]])
+    ]], dtype=np.float32)
+    
     scaled_data = scaler.transform(input_data)
-    prediction = model.predict(scaled_data)
-    probability = float(prediction[0][0])
+    
+    infer = model.signatures["serving_default"]
+    prediction = infer(tf.convert_to_tensor(scaled_data))
+    
+    probability = float(list(prediction.values())[0].numpy()[0][0])
+    
     return {"diabetes_probability": probability, "is_diabetic": bool(probability > 0.5)}
